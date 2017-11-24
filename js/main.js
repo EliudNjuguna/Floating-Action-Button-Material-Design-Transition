f7956766e4ddab7082a2c1bb5a03f63a14341493
Eliud Njuguna Kamau <njugunael@gmail.com>
2017-11-24 23:26:47 +0300

    initial commit
    Floating Action Bar Material Design Transition

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.trigger').click(function(){
+        $(this).addClass('open');
+        $('.pseudo-circle').addClass('open');
+    });
+    $('.close').click(function(){
+        $('.trigger').removeClass('open');
+        $('.pseudo-circle').removeClass('open');
+    });
+});
